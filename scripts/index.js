16e957b1b9d1bcf49482c13140eabaabdc1736d9
Ajhat22 <131907314+Ajhat22@users.noreply.github.com>
2025-05-26 03:47:12 +0000

    part 1 java script

---
+++ scripts/index.js
@@ -0,0 +1,40 @@
+//this is for profile
+
+const editProfileButton = document.querySelector(".profile__edit-button");
+
+const profileModal = document.querySelector("#edit-profile-modal");
+
+const profileCloseButton = profileModal.querySelector(".modal__close-button");
+
+  //this is for post modal
+
+const postButton = document.querySelector(".post__post-button");
+
+const postModal = document.querySelector("#new-post-modal");
+
+const postCloseButton = postModal.querySelector(".modal__close-button");
+//events for profile
+
+editProfileButton.addEventListener("click", function () {
+ 
+  profileModal.classList.add("modal_is-opened");
+
+
+});
+
+profileCloseButton.addEventListener("click", function(){
+  profileModal.classList.remove("modal_is-opened");
+});
+
+//events for new post
+
+postButton.addEventListener("click", function () {
+ 
+  postModal.classList.add("modal_is-opened");
+
+
+});
+
+postCloseButton.addEventListener("click", function(){
+  profileModal.classList.remove("modal_is-opened");
+});
