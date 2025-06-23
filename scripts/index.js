48b1f9b7ffada92e0b9f7c4912068206fe900453
ajs brim <ajhatcher112@gmail.com>
2025-06-23 06:53:53 -0500

    the lovely arrays

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,20 @@
+//arrays for cards (notes are for me later)
+const initialCards = [
+{ name:"Val Thorens",
+link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/1-photo-by-moritz-feldmann-from-pexels.jpg" },
+{ name: "Restaurant terrace",
+link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/2-photo-by-ceiline-from-pexels.jpg" },
+{name: "An outdoor cafe",
+link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/3-photo-by-tubanur-dogan-from-pexels.jpg"},
+{name: "A very long bridge, over the forest and through the trees",
+link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/4-photo-by-maurice-laschet-from-pexels.jpg"},
+{name: "Tunnel with morning light",
+link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/5-photo-by-van-anh-nguyen-from-pexels.jpg"},
+{name: "Mountain house",
+link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/6-photo-by-moritz-feldmann-from-pexels.jpg"},
+];
+
+
 //this is for profile constant basic (notes are more for me later)
 
 const editProfileButton = document.querySelector(".profile__edit-button");
@@ -81,3 +98,12 @@ function handlePostSave(evt) {
 }
 
 postSave.addEventListener("submit", handlePostSave);
+
+
+
+//info for card arrays
+
+initialCards.forEach(function (item) {
+console.log(item.name);
+console.log(item.link);
+});
